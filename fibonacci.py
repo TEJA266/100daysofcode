891a8dc4292345f10945b4a517d5976787ad1278
Teja vardhan <62698692+TEJA266@users.noreply.github.com>
2021-07-14 22:59:32 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,7 @@
 def fib(n):
-    if n==0 or n==1:
+    if n==0:
+        return o
+    elif n==1:
         return 1 
     else:
         return fib(n-1)+fib(n-2)
@@ -7,3 +9,20 @@ n=int(input())
 print("Fibonacci series upto given no of times is:")
 for n in range(0,n):
     print(fib(n),end=" ")
+    
+    
+    
+    
+# without recursion
+n = int(input("Enter the value of 'n': "))
+a = 0
+b = 1
+sum = 0
+count = 1
+print("Fibonacci Series: ", end = " ")
+while(count <= n):
+  print(sum, end = " ")
+  count += 1
+  a = b
+  b = sum
+  sum = a + b
